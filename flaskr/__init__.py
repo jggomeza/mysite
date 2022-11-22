8316fe9ebd20ce6ab6f022cf0052cf24049cfd32
jggomeza <ing.jggomeza@gmail.com>
2022-11-21 22:34:05 -0400

    Se modifico la configuración de la app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,30 +1,32 @@
 import os
-
+import sys
 from flask import Flask
+sys.path.append("..")
+sys.path.append(".")
 
+# # print (os.getcwd())
+
+# Packages
+from instance.config import config
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
+    if test_config is not None:
+        app.config.from_mapping(test_config)    
+    
+    app.config.from_object(config['default'])
+    # for i in app.config:
+    #     print(f"{i}: {app.config[i]}")
+  
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
+    # # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
@@ -44,7 +46,7 @@ def create_app(test_config=None):
 # Run app
 # flask --app flaskr --debug run
 # flask --app flaskr --debug run --host=0.0.0.0
-# flask --app flaskr --debug run --host=0.0.0.0 --port=8080
+# flask --app flaskr --debug run --host=0.0.0.0 --port=80
 # flask --app flaskr init-db
 
 # Install the Project
@@ -69,5 +71,3 @@ def create_app(test_config=None):
 # Run with a Production Server
 # pip install waitress
 # waitress-serve --call 'flaskr:create_app'
-
-# esto es un cambio para develop
