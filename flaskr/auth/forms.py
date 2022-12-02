2d4762dbafcb9790425bb3737d1dfb9ffe42e9f5
jggomeza <ing.jggomeza@gmail.com>
2022-12-02 01:57:10 -0400

    Add news changes

--- flaskr/auth/forms.py
+++ flaskr/auth/forms.py
@@ -1,6 +1,5 @@
 # Librerias de Formularios
-from wtforms import Form
-from wtforms import StringField, BooleanField, PasswordField, SubmitField
+from wtforms import Form, BooleanField, DateField, DateTimeField, DecimalField, FileField, MultipleFileField, FloatField, IntegerField, RadioField, SelectField, SelectMultipleField, SubmitField, StringField, HiddenField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired
 
 class FormLogin(Form):
